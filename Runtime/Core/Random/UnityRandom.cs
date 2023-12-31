dc33e74ac884cc9925afca2b47356ee472ba914f
wallstop <wallstop@wallstopstudios.com>
2023-12-30 17:54:10 -0800

    Add UnityRandom

---
+++ Runtime/Core/Random/UnityRandom.cs
@@ -0,0 +1,24 @@
+﻿namespace UnityHelpers.Core.Random
+{
+    using System;
+
+    public sealed class UnityRandom : AbstractRandom
+    {
+        public static readonly UnityRandom Instance = new UnityRandom();
+
+        private UnityRandom()
+        {
+        }
+
+        public override RandomState InternalState => throw new NotSupportedException("Unity Random does not expose its internal state");
+        public override uint NextUint()
+        {
+            return unchecked((uint)UnityEngine.Random.Range(int.MinValue, int.MaxValue));
+        }
+
+        public override IRandom Copy()
+        {
+            throw new NotSupportedException("Unity Random does not support copying / seeding");
+        }
+    }
+}
---
+++ Tests/Runtime/Scripts/UnityRandomTests.cs
@@ -0,0 +1,9 @@
+﻿namespace UnityHelpers.Tests
+{
+    using Core.Random;
+
+    public sealed class UnityRandomTests : RandomTestBase
+    {
+        protected override IRandom NewRandom() => UnityRandom.Instance;
+    }
+}
