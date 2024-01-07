58164f24fdf5b3d7547a141812cd5f88d2efa956
wallstop <wallstop@wallstopstudios.com>
2024-01-07 14:39:21 -0800

    Remove unused namespace

--- Runtime/Core/Extension/UnityExtensions.cs
+++ Runtime/Core/Extension/UnityExtensions.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Xml.Linq;
     using DataStructure;
     using DataStructure.Adapters;
     using Helper;
