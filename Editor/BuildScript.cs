f835a557915dc79a8317fd1a56493c851c166ca8
Eli Pinkerton <wallstop@wallstopstudios.com>
2025-03-15 18:44:23 -0700

    Maybe fix build

---
+++ Editor/BuildScript.cs
@@ -0,0 +1,30 @@
+﻿namespace UnityHelpers.Editor
+{
+#if UNITY_EDITOR
+    using UnityEditor;
+    using System.IO;
+
+    // Needed for build
+    public static class BuildScript
+    {
+        [MenuItem("Build/Build Unity Package")]
+        public static void BuildProject()
+        {
+            string[] scenes = { "Editor/Scenes/SampleScene.unity" };
+
+            const string buildPath = "Builds/UnityHelpers";
+            if (!Directory.Exists(buildPath))
+            {
+                Directory.CreateDirectory(buildPath);
+            }
+
+            BuildPipeline.BuildPlayer(
+                scenes,
+                buildPath + "/UnityHelpers.x86_64",
+                BuildTarget.StandaloneLinux64,
+                BuildOptions.None
+            );
+        }
+    }
+#endif
+}
