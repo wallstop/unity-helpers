e33b46f7f13ddf71d1d15ae93d78761bbd942227
Eli Pinkerton <wallstop@wallstopstudios.com>
2025-04-06 14:04:15 -0700

    UnityMainThreadDispatcher now runs in Edit mode

--- Runtime/Core/Helper/UnityMainThreadDispatcher.cs
+++ Runtime/Core/Helper/UnityMainThreadDispatcher.cs
@@ -2,16 +2,64 @@
 {
     using System;
     using System.Collections.Concurrent;
+    using UnityEditor;
     using UnityEngine;
     using Utils;
 
+    [ExecuteAlways]
     public sealed class UnityMainThreadDispatcher : RuntimeSingleton<UnityMainThreadDispatcher>
     {
         private readonly ConcurrentQueue<Action> _actions = new();
 
+#if UNITY_EDITOR
+        private readonly EditorApplication.CallbackFunction _update;
+        private bool _attachedEditorUpdate;
+#endif
+
+        public UnityMainThreadDispatcher()
+        {
+#if UNITY_EDITOR
+            _update = Update;
+#endif
+        }
+
         public void RunOnMainThread(Action action)
         {
             _actions.Enqueue(action);
+        }
+
+        private void OnEnable()
+        {
+#if UNITY_EDITOR
+            if (!_attachedEditorUpdate)
+            {
+                EditorApplication.update += _update;
+                _attachedEditorUpdate = true;
+            }
+#endif
+        }
+
+        private void OnDisable()
+        {
+#if UNITY_EDITOR
+            if (_attachedEditorUpdate)
+            {
+                EditorApplication.update -= _update;
+                _attachedEditorUpdate = false;
+            }
+#endif
+        }
+
+        protected override void OnDestroy()
+        {
+#if UNITY_EDITOR
+            if (_attachedEditorUpdate)
+            {
+                EditorApplication.update -= _update;
+                _attachedEditorUpdate = false;
+            }
+#endif
+            base.OnDestroy();
         }
 
         private void Update()
