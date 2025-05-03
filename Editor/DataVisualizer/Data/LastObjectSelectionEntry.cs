7fc1e0274c13258251830b68b831df1c57fb3f7d
Eli Pinkerton <wallstop@wallstopstudios.com>
2025-05-03 12:11:42 -0700

    Remove data visualizer

--- Editor/DataVisualizer/Data/LastObjectSelectionEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WallstopStudios.UnityHelpers.Editor.DataVisualizer.Data
-{
-    [System.Serializable]
-    public class LastObjectSelectionEntry
-    {
-        public string TypeName;
-        public string ObjectGuid;
-    }
-}
