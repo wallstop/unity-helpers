e36b4e14265685205bf4da2ad51873fa01f1e634
Eli Pinkerton <wallstop@wallstopstudios.com>
2025-05-05 15:27:56 -0700

    Fix SpriteCropper

--- Editor/SpriteCropper.cs
+++ Editor/SpriteCropper.cs
@@ -3,11 +3,10 @@
 #if UNITY_EDITOR
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel;
     using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
-    using Core.Helper;
+    using Core.Extension;
     using UnityEditor;
     using UnityEngine;
     using Object = UnityEngine.Object;
@@ -33,6 +32,11 @@
         [SerializeField]
         private Object[] _inputDirectories;
 
+        [SerializeField]
+        private bool _onlyNecessary;
+
+        private List<string> _filesToProcess;
+
         [MenuItem("Tools/Wallstop Studios/Unity Helpers/" + Name)]
         private static void ShowWindow() => GetWindow<SpriteCropper>(Name);
 
@@ -42,48 +46,118 @@
             SerializedObject so = new(this);
             SerializedProperty dirs = so.FindProperty(nameof(_inputDirectories));
             EditorGUILayout.PropertyField(dirs, true);
+            SerializedProperty onlyNecessary = so.FindProperty(nameof(_onlyNecessary));
+            EditorGUILayout.PropertyField(onlyNecessary, true);
             so.ApplyModifiedProperties();
 
-            if (GUILayout.Button("Process Sprites"))
+            if (GUILayout.Button("Find Sprites To Process"))
             {
-                List<string> allFiles = new();
-                foreach (Object maybeDirectory in _inputDirectories.Where(Objects.NotNull))
+                FindFilesToProcess();
+            }
+
+            if (_filesToProcess != null && _filesToProcess.Count > 0)
+            {
+                GUILayout.Label(
+                    $"Found {_filesToProcess.Count} sprites to process.",
+                    EditorStyles.boldLabel
+                );
+                if (GUILayout.Button($"Process {_filesToProcess.Count} Sprites"))
                 {
-                    string assetPath = AssetDatabase.GetAssetPath(maybeDirectory);
-                    if (!AssetDatabase.IsValidFolder(assetPath))
+                    ProcessFoundSprites();
+                    _filesToProcess = null;
+                }
+            }
+            else if (_filesToProcess != null)
+            {
+                GUILayout.Label(
+                    "No sprites found to process in the selected directories.",
+                    EditorStyles.label
+                );
+            }
+        }
+
+        private void FindFilesToProcess()
+        {
+            _filesToProcess = new List<string>();
+            if (_inputDirectories == null || _inputDirectories.Length == 0)
+            {
+                this.LogWarn($"No input directories selected.");
+                return;
+            }
+
+            foreach (Object maybeDirectory in _inputDirectories.Where(d => d != null))
+            {
+                string assetPath = AssetDatabase.GetAssetPath(maybeDirectory);
+                if (!AssetDatabase.IsValidFolder(assetPath))
+                {
+                    this.LogWarn($"Skipping invalid path: {assetPath}");
+                    continue;
+                }
+
+                IEnumerable<string> files = Directory
+                    .GetFiles(assetPath, "*.*", SearchOption.AllDirectories)
+                    .Where(file =>
+                        Array.Exists(
+                            ImageFileExtensions,
+                            extension =>
+                                file.EndsWith(extension, StringComparison.OrdinalIgnoreCase)
+                        )
+                    );
+
+                foreach (string file in files)
+                {
+                    if (file.Contains(CroppedPrefix, StringComparison.OrdinalIgnoreCase))
                     {
                         continue;
                     }
+                    _filesToProcess.Add(file);
+                }
+            }
+            Repaint();
+        }
 
-                    IEnumerable<string> files = Directory
-                        .GetFiles(assetPath, "*.*", SearchOption.AllDirectories)
-                        .Where(file =>
-                            Array.Exists(
-                                ImageFileExtensions,
-                                extension =>
-                                    file.EndsWith(extension, StringComparison.OrdinalIgnoreCase)
-                            )
-                        );
+        private void ProcessFoundSprites()
+        {
+            if (_filesToProcess == null || _filesToProcess.Count == 0)
+            {
+                this.LogWarn($"No files found or selected for processing.");
+                return;
+            }
 
-                    foreach (string file in files)
+            string lastProcessed = null;
+            try
+            {
+                int total = _filesToProcess.Count;
+                List<TextureImporter> newImporters = new();
+                AssetDatabase.StartAssetEditing();
+                try
+                {
+                    for (int i = 0; i < _filesToProcess.Count; ++i)
                     {
-                        if (file.Contains(CroppedPrefix, StringComparison.OrdinalIgnoreCase))
-                        {
-                            continue;
-                        }
-
-                        allFiles.Add(file);
+                        string file = _filesToProcess[i];
+                        lastProcessed = file;
+                        EditorUtility.DisplayProgressBar(
+                            Name,
+                            $"Pre-processing {i + 1}/{total}: {Path.GetFileName(file)}",
+                            i / (float)total
+                        );
+                        CheckPreProcessNeeded(file);
                     }
+                }
+                finally
+                {
+                    AssetDatabase.StopAssetEditing();
+                    AssetDatabase.SaveAssets();
+                    AssetDatabase.Refresh();
                 }
 
                 AssetDatabase.StartAssetEditing();
                 try
                 {
-                    int total = allFiles.Count;
-                    List<TextureImporter> newImporters = new();
-                    for (int i = 0; i < allFiles.Count; ++i)
+                    for (int i = 0; i < _filesToProcess.Count; ++i)
                     {
-                        string file = allFiles[i];
+                        string file = _filesToProcess[i];
+                        lastProcessed = file;
                         EditorUtility.DisplayProgressBar(
                             Name,
                             $"Processing {i + 1}/{total}: {Path.GetFileName(file)}",
@@ -100,18 +174,57 @@
                     {
                         newImporter.SaveAndReimport();
                     }
+                }
+                finally
+                {
                     AssetDatabase.StopAssetEditing();
                     AssetDatabase.SaveAssets();
                     AssetDatabase.Refresh();
                 }
-                finally
-                {
-                    EditorUtility.ClearProgressBar();
-                }
+
+                this.Log($"{newImporters.Count} sprites processed successfully.");
+            }
+            catch (Exception e)
+            {
+                this.LogError(
+                    $"An error occurred during processing. Last processed: {lastProcessed}.",
+                    e
+                );
+                AssetDatabase.StopAssetEditing();
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
             }
         }
 
-        private static TextureImporter ProcessSprite(string assetPath)
+        private static void CheckPreProcessNeeded(string assetPath)
+        {
+            string assetDirectory = Path.GetDirectoryName(assetPath);
+            if (string.IsNullOrWhiteSpace(assetDirectory))
+            {
+                return;
+            }
+
+            TextureImporter importer = AssetImporter.GetAtPath(assetPath) as TextureImporter;
+            if (importer == null || !importer.textureType.Equals(TextureImporterType.Sprite))
+            {
+                return;
+            }
+
+            TextureImporterSettings originalSettings = new();
+            importer.ReadTextureSettings(originalSettings);
+
+            if (!importer.isReadable)
+            {
+                importer.isReadable = true;
+                importer.SaveAndReimport();
+            }
+        }
+
+        private TextureImporter ProcessSprite(string assetPath)
         {
             string assetDirectory = Path.GetDirectoryName(assetPath);
             if (string.IsNullOrWhiteSpace(assetDirectory))
@@ -128,6 +241,7 @@
             TextureImporterSettings originalSettings = new();
             importer.ReadTextureSettings(originalSettings);
 
+            bool originalReadableState = importer.isReadable;
             if (!importer.isReadable)
             {
                 importer.isReadable = true;
@@ -135,101 +249,146 @@
             }
 
             Texture2D tex = AssetDatabase.LoadAssetAtPath<Texture2D>(assetPath);
-            Color32[] pixels = tex.GetPixels32();
+            if (tex == null)
+                return null;
 
-            int width = tex.width;
-            int height = tex.height;
-            int minX = width;
-            int minY = height;
-            int maxX = 0;
-            int maxY = 0;
-            bool hasVisible = false;
+            TextureImporter resultImporter;
+            try
+            {
+                Color32[] pixels = tex.GetPixels32();
 
-            Parallel.For(
-                0,
-                width * height,
-                index =>
-                {
-                    int x = index % width;
-                    int y = index / width;
+                int width = tex.width;
+                int height = tex.height;
+                int minX = width;
+                int minY = height;
+                int maxX = 0;
+                int maxY = 0;
+                bool hasVisible = false;
 
-                    float a = pixels[index].a / 255f;
-                    if (a <= AlphaThreshold)
+                object lockObject = new();
+
+                Parallel.For(
+                    0,
+                    width * height,
+                    () => (minX: width, minY: height, maxX: 0, maxY: 0, hasVisible: false),
+                    (index, _, localState) =>
                     {
-                        return;
+                        int x = index % width;
+                        int y = index / width;
+
+                        float a = pixels[index].a / 255f;
+                        if (a > AlphaThreshold)
+                        {
+                            localState.hasVisible = true;
+                            localState.minX = Mathf.Min(localState.minX, x);
+                            localState.minY = Mathf.Min(localState.minY, y);
+                            localState.maxX = Mathf.Max(localState.maxX, x);
+                            localState.maxY = Mathf.Max(localState.maxY, y);
+                        }
+                        return localState;
+                    },
+                    finalLocalState =>
+                    {
+                        if (finalLocalState.hasVisible)
+                        {
+                            lock (lockObject)
+                            {
+                                hasVisible = true;
+                                minX = Mathf.Min(minX, finalLocalState.minX);
+                                minY = Mathf.Min(minY, finalLocalState.minY);
+                                maxX = Mathf.Max(maxX, finalLocalState.maxX);
+                                maxY = Mathf.Max(maxY, finalLocalState.maxY);
+                            }
+                        }
                     }
+                );
 
-                    hasVisible = true;
-                    minX = Mathf.Min(minX, x);
-                    minY = Mathf.Min(minY, y);
-                    maxX = Mathf.Max(maxX, x);
-                    maxY = Mathf.Max(maxY, y);
-                }
-            );
-
-            if (!hasVisible)
-            {
-                return null;
-            }
-
-            int cropWidth = maxX - minX + 1;
-            int cropHeight = maxY - minY + 1;
-            Texture2D cropped = new(cropWidth, cropHeight, TextureFormat.RGBA32, false);
-
-            Color32[] croppedPixels = new Color32[cropWidth * cropHeight];
-            Parallel.For(
-                0,
-                croppedPixels.Length,
-                index =>
+                if (!hasVisible)
                 {
-                    int x = index % cropWidth;
-                    int y = index / cropWidth;
-                    int outputX = x + minX;
-                    int outputY = y + minY;
-                    croppedPixels[index] = pixels[outputY * width + outputX];
+                    return null;
                 }
-            );
-            cropped.SetPixels32(croppedPixels);
-            cropped.Apply();
 
-            string newPath = Path.Combine(
-                assetDirectory,
-                CroppedPrefix + Path.GetFileName(assetPath)
-            );
-            File.WriteAllBytes(newPath, cropped.EncodeToPNG());
-            AssetDatabase.ImportAsset(newPath);
-            TextureImporter newImporter = AssetImporter.GetAtPath(newPath) as TextureImporter;
-            if (newImporter == null)
+                int cropWidth = maxX - minX + 1;
+                int cropHeight = maxY - minY + 1;
+
+                if (_onlyNecessary && cropWidth == width && cropHeight == height)
+                {
+                    return null;
+                }
+
+                Texture2D cropped = new(cropWidth, cropHeight, TextureFormat.RGBA32, false);
+                Color32[] croppedPixels = new Color32[cropWidth * cropHeight];
+
+                Parallel.For(
+                    0,
+                    cropHeight,
+                    y =>
+                    {
+                        int sourceYOffset = (y + minY) * width;
+                        int destYOffset = y * cropWidth;
+                        for (int x = 0; x < cropWidth; ++x)
+                        {
+                            croppedPixels[destYOffset + x] = pixels[sourceYOffset + x + minX];
+                        }
+                    }
+                );
+
+                cropped.SetPixels32(croppedPixels);
+                cropped.Apply();
+
+                string newPath = Path.Combine(
+                    assetDirectory,
+                    CroppedPrefix + Path.GetFileName(assetPath)
+                );
+                File.WriteAllBytes(newPath, cropped.EncodeToPNG());
+                DestroyImmediate(cropped);
+                AssetDatabase.ImportAsset(newPath);
+                TextureImporter newImporter = AssetImporter.GetAtPath(newPath) as TextureImporter;
+                if (newImporter == null)
+                {
+                    return null;
+                }
+
+                newImporter.textureType = importer.textureType;
+                newImporter.spriteImportMode = importer.spriteImportMode;
+                newImporter.filterMode = importer.filterMode;
+                newImporter.textureCompression = importer.textureCompression;
+                newImporter.wrapMode = importer.wrapMode;
+                newImporter.mipmapEnabled = importer.mipmapEnabled;
+                newImporter.spritePixelsPerUnit = importer.spritePixelsPerUnit;
+
+                TextureImporterSettings newSettings = new();
+                newImporter.ReadTextureSettings(newSettings);
+                newSettings.spriteExtrude = originalSettings.spriteExtrude;
+
+                Vector2 origPivot = GetSpritePivot(importer);
+                Vector2 origCenter = new(width * origPivot.x, height * origPivot.y);
+                Vector2 newPivotPixels = origCenter - new Vector2(minX, minY);
+                Vector2 newPivotNorm = new(
+                    cropWidth > 0 ? newPivotPixels.x / cropWidth : 0.5f,
+                    cropHeight > 0 ? newPivotPixels.y / cropHeight : 0.5f
+                );
+
+                newImporter.spriteImportMode = SpriteImportMode.Single;
+                newImporter.spritePivot = newPivotNorm;
+                newSettings.spritePivot = newPivotNorm;
+                newSettings.spriteAlignment = (int)SpriteAlignment.Custom;
+
+                newImporter.SetTextureSettings(newSettings);
+                newImporter.isReadable = false;
+
+                resultImporter = newImporter;
+            }
+            finally
             {
-                return null;
+                if (importer != null && importer.isReadable != originalReadableState)
+                {
+                    importer.isReadable = originalReadableState;
+                    importer.SaveAndReimport();
+                }
             }
 
-            newImporter.textureType = importer.textureType;
-            newImporter.spriteImportMode = importer.spriteImportMode;
-            newImporter.filterMode = importer.filterMode;
-            newImporter.textureCompression = importer.textureCompression;
-            newImporter.wrapMode = importer.wrapMode;
-            newImporter.mipmapEnabled = importer.mipmapEnabled;
-            newImporter.spritePixelsPerUnit = importer.spritePixelsPerUnit;
-
-            TextureImporterSettings newSettings = new();
-            newImporter.ReadTextureSettings(newSettings);
-            newSettings.spriteExtrude = originalSettings.spriteExtrude;
-
-            Vector2 origPivot = GetSpritePivot(importer);
-            Vector2 origCenter = new(tex.width * origPivot.x, tex.height * origPivot.y);
-            Vector2 newPivotPixels = origCenter - new Vector2(minX, minY);
-            Vector2 newPivotNorm = new(newPivotPixels.x / cropWidth, newPivotPixels.y / cropHeight);
-
-            newImporter.spriteImportMode = SpriteImportMode.Single;
-            newImporter.spritePivot = newPivotNorm;
-            newSettings.spritePivot = newPivotNorm;
-            newSettings.spriteAlignment = (int)SpriteAlignment.Custom;
-
-            newImporter.SetTextureSettings(newSettings);
-            newImporter.isReadable = true;
-
-            return newImporter;
+            return resultImporter;
         }
 
         private static Vector2 GetSpritePivot(TextureImporter importer)
@@ -238,11 +397,8 @@
             {
                 return importer.spritePivot;
             }
-            throw new InvalidEnumArgumentException(
-                nameof(importer.spriteImportMode),
-                (int)importer.spriteImportMode,
-                typeof(SpriteImportMode)
-            );
+
+            return new Vector2(0.5f, 0.5f);
         }
     }
 #endif
