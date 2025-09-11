6ae6f7cc74da238b1533d898fe9ce37efd77a1c7
wallstop <wallstop@wallstopstudios.com>
2025-09-11 11:07:39 -0700

    Add IndexOf for IReadonlyList

---
+++ Runtime/Core/Extension/IReadonlyListExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace WallstopStudios.UnityHelpers.Core.Extension
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class IReadonlyListExtensions
+    {
+        public static int IndexOf<T>(this IReadOnlyList<T> readonlyList, T element)
+        {
+            switch (readonlyList)
+            {
+                case T[] array:
+                {
+                    return Array.IndexOf(array, element);
+                }
+                case List<T> list:
+                {
+                    return list.IndexOf(element);
+                }
+                default:
+                {
+                    // TODO: Configurable?
+                    EqualityComparer<T> comparer = EqualityComparer<T>.Default;
+                    for (int i = 0; i < readonlyList.Count; i++)
+                    {
+                        if (comparer.Equals(readonlyList[i], element))
+                        {
+                            return i;
+                        }
+                    }
+
+                    return -1;
+                }
+            }
+        }
+    }
+}
---
+++ Tests/Runtime/Extensions/IReadonlyListExtensionTests.cs
@@ -0,0 +1,58 @@
+﻿namespace WallstopStudios.UnityHelpers.Tests.Extensions
+{
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using WallstopStudios.UnityHelpers.Core.DataStructure;
+    using WallstopStudios.UnityHelpers.Core.Extension;
+
+    public sealed class IReadonlyListExtensionTests
+    {
+        [Test]
+        public void IndexOfArray()
+        {
+            IReadOnlyList<int> list = new int[] { 1, 2, 3 };
+            int index = list.IndexOf(1);
+            Assert.AreEqual(0, index);
+            index = list.IndexOf(2);
+            Assert.AreEqual(1, index);
+            index = list.IndexOf(3);
+            Assert.AreEqual(2, index);
+            index = list.IndexOf(4);
+            Assert.IsTrue(index < 0);
+            index = list.IndexOf(-1);
+            Assert.IsTrue(index < 0);
+        }
+
+        [Test]
+        public void IndexOfList()
+        {
+            IReadOnlyList<int> list = new List<int>() { 1, 2, 3 };
+            int index = list.IndexOf(1);
+            Assert.AreEqual(0, index);
+            index = list.IndexOf(2);
+            Assert.AreEqual(1, index);
+            index = list.IndexOf(3);
+            Assert.AreEqual(2, index);
+            index = list.IndexOf(4);
+            Assert.IsTrue(index < 0);
+            index = list.IndexOf(-1);
+            Assert.IsTrue(index < 0);
+        }
+
+        [Test]
+        public void IndexOfCyclicBuffer()
+        {
+            CyclicBuffer<int> list = new(3) { 1, 2, 3 };
+            int index = list.IndexOf(1);
+            Assert.AreEqual(0, index);
+            index = list.IndexOf(2);
+            Assert.AreEqual(1, index);
+            index = list.IndexOf(3);
+            Assert.AreEqual(2, index);
+            index = list.IndexOf(4);
+            Assert.IsTrue(index < 0);
+            index = list.IndexOf(-1);
+            Assert.IsTrue(index < 0);
+        }
+    }
+}
